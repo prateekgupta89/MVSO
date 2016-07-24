918220a71803c6f7dd4ed12a08909f3a318b6898
pg2512 <pg2512@columbia.edu>
2016-07-24 15:08:43 -0400

    Latest commit

--- Results.py
+++ Results.py
@@ -4,20 +4,22 @@ from datetime import datetime
 import csv
 
 def zerolistmaker(n):
-    listofzeros = [0] * n
+    listofzeros = [0.0] * n
     return listofzeros
 
-def formAns(ans_obj):
-    ans_tuple = map(lambda a: (a.qid, a.fields), ans_obj)
+def formAns(ans_obj, ANP):
+    ans_tuple = map(lambda a: (ANP, a.fields), ans_obj)
     return dict(ans_tuple)
 
-def parseAnswer(form, ANP_set):
+def parseAnswer(form, hit_anp_dict):
     # Create a dictionary to hold form responses
     form_dict = {}
 
     # Stick form response in a dict
     for a in form.answers:
-        form_ans = formAns(a)
+        hitId = form.HITId
+        ANP = hit_anp_dict[hitId]
+        form_ans = formAns(a, ANP)
         for r in form_ans.keys():
             form_dict[r] = list()
             for value in form_ans.values():
@@ -26,16 +28,16 @@ def parseAnswer(form, ANP_set):
     # Form responses where no data is recorded will have a None value
     return form_dict
 
-def reviewAssignment(a, ANP_set, mturk=None, auto_approve=True, redownload=False):
+def reviewAssignment(a, hit_anp_dict, mturk=None, auto_approve=False, redownload=False):
     answer_list = list()
     aid = a.AssignmentId
     status = a.AssignmentStatus
     if status == "Submitted":
         if auto_approve:
             mtc.approve_assignment(aid)
-        answer_list.append(parseAnswer(a, ANP_set))
+        answer_list.append(parseAnswer(a, hit_anp_dict))
     if a.AssignmentStatus == "Approved" and redownload:
-        answer_list.append(parseAnswer(a, ANP_set))
+        answer_list.append(parseAnswer(a, hit_anp_dict))
     return answer_list
 
 if __name__ == '__main__':
@@ -66,6 +68,17 @@ if __name__ == '__main__':
               'Anticipation',
               'Interest']
 
+    ANP_set = set()
+    reader = csv.reader(open('dict.csv'))
+    hit_anp_dict = {}
+    for row in reader:
+        key = row[0]
+        if key in hit_anp_dict:
+        # implement your duplicate row handling here
+            pass
+        hit_anp_dict[key] = row[1]
+        ANP_set.add(row[1])
+
     ratings = range(1,25)
 
     d = dict(zip(emotions, ratings))
@@ -84,25 +97,15 @@ if __name__ == '__main__':
                           aws_secret_access_key=SECRET_KEY,
                           host=HOST)
 
-    ANP_set = set()
-    f = open('testANP.csv')
-    csv_f = csv.reader(f)
-
-    for row in csv_f:
-        ANP_set.add(row[0])
-
-    ANP_set.add('design')
-
     # Download reviewable HITs
     reviewable_assignments = list()
-    hits = mtc.get_all_hits()
-    hit_list = map(lambda h: h, hits)
+    hit_list = list(hit_anp_dict.keys())
     for h in hit_list:
         max_page = False
         m = 10
         p = 1
         while not max_page:
-            assignments = mtc.get_assignments(h.HITId, page_size=m, page_number=p)
+            assignments = mtc.get_assignments(h, page_size=m, page_number=p)
             if len(assignments) > 0:
                 reviewable_assignments.extend(assignments)
                 if(len(assignments) < m):
@@ -115,10 +118,11 @@ if __name__ == '__main__':
     # Review assignments
     response_list = list()
     for a in reviewable_assignments:
-        submitted_work = reviewAssignment(a, ANP_set, mtc, redownload=True)
+        submitted_work = reviewAssignment(a, hit_anp_dict, mtc, redownload=True)
         if len(submitted_work) > 0:
             response_list.append(submitted_work)
     responses = list(chain.from_iterable(response_list))
+    print responses
 
     anp_keys = map(lambda i: i,  ANP_set)
     anp_dict = dict.fromkeys(anp_keys, None)
@@ -129,8 +133,10 @@ if __name__ == '__main__':
         if cur_key[0] in anp_dict:
             anp_dict[cur_key[0]] = zerolistmaker(len(emotions))
             for value in cur_val[0][0]:
-                val = int(value)
-                anp_dict[cur_key[0]][val-1] += 1 
+                emotion = value.split('|')
+                for i in range(0, len(emotion)):
+                    val = d[emotion[i]]
+                    anp_dict[cur_key[0]][val-1] += 1
 
     anp_emotions_list = emotions
     anp_emotions_list.insert(0, 'ANP')
